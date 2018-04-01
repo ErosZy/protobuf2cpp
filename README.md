276ccd274a92dbb8ff16b17ff41592a2f2acaf5f
zyEros <zhaoyang@qmtv.com>
2018-04-01 15:30:45 +0800

    UPD

---
+++ README.md
@@ -0,0 +1,3 @@
+Protobuf2cpp
+===
+读取protobuf文件，执行token stream分析，生成cpp的包装代码，提供给wasm使用
---
+++ main.js
@@ -0,0 +1,8 @@
+const Token = require("./token");
+const Parser = require("./parser");
+const Protobuf2cpp = require('./protobuf2cpp');
+
+let tokenizer = new Token("./protocol/pro.proto");
+let parser = new Parser(tokenizer);
+let protobuf2cpp = new Protobuf2cpp(parser);
+protobuf2cpp.convert();
